3d7f00d17546a27e02a18289fe3436cc9e388d48
hanifahzakiyyah <hanifahzakiyyatus@gmail.com>
2025-01-23 09:39:45 +0700

    first

---
+++ src/Experience.jsx
@@ -0,0 +1,107 @@
+import { OrbitControls, Sparkles } from '@react-three/drei'
+import BoxBaru from './BoxBaru'
+import { useEffect, useRef, useState } from 'react'
+import { useHelper } from '@react-three/drei'
+import { DirectionalLightHelper, AxesHelper } from 'three'
+
+export default function Experience() {
+    const light1Ref = useRef()
+    const light2Ref = useRef()
+    const [showOrbCont, setShowOrbCont] = useState(false)
+
+
+    //pengaturan orbit control
+    useEffect(()=>{
+        const elemen = document.getElementById("root")
+        if (elemen?.classList.contains("page-index")) {
+            setShowOrbCont(true)
+        } else{
+            setShowOrbCont(false)
+        }
+    }, [])
+
+    // Menggunakan helper untuk directionalLight
+    // useHelper(light1Ref, DirectionalLightHelper, 1)
+    // useHelper(light2Ref, DirectionalLightHelper, 1)
+
+    return (
+        <>
+            {showOrbCont && <OrbitControls 
+                makeDefault 
+                enableZoom={false} 
+                enableDamping 
+                maxPolarAngle={Math.PI/2}
+                maxAzimuthAngle={Math.PI/2}
+            />}
+            
+
+            {/* Pencahayaan */}
+            <directionalLight
+                
+                castShadow
+                position={[3, 4, 2]}
+                intensity={0.5}
+                shadow-camera-near={0.5}
+                shadow-camera-far={50}
+                shadow-camera-left={-10}
+                shadow-camera-right={10}
+                shadow-camera-top={10}
+                shadow-camera-bottom={-10}
+            />
+            <directionalLight
+                ref={light2Ref}
+                castShadow
+                position={[-4, 5, 2]}
+                intensity={0.5}
+                shadow-camera-near={0.5}
+                shadow-camera-far={50}
+                shadow-camera-left={-10}
+                shadow-camera-right={10}
+                shadow-camera-top={10}
+                shadow-camera-bottom={-10}
+            />
+            {/* <directionalLight
+                castShadow
+                position={[-4, 0, -1]}
+                intensity={0.2}s
+            /> */}
+            <directionalLight
+                ref={light1Ref}
+                castShadow
+                position={[3, 4, -3]}
+                intensity={0.7}
+                shadow-camera-near={0.5}
+                shadow-camera-far={50}
+                shadow-camera-left={-10}
+                shadow-camera-right={10}
+                shadow-camera-top={10}
+                shadow-camera-bottom={-10}
+            />
+            <directionalLight
+                castShadow
+                position={[0, 0, -3]}
+                intensity={0.2}
+                shadow-camera-near={0.5}
+                shadow-camera-far={50}
+                shadow-camera-left={-10}
+                shadow-camera-right={10}
+                shadow-camera-top={10}
+                shadow-camera-bottom={-10}
+            />
+            <ambientLight intensity={0.4} /> 
+
+            {/* Helper untuk memvisualisasikan posisi */}
+            {/* <primitive object={new AxesHelper(5)} /> */}
+
+            {/* Objek lain */}
+            {/* <Box /> */}
+            <BoxBaru/>
+
+            <Sparkles
+                size={20}
+                scale={[30,30,30]}
+                count={250}
+            />
+        </>
+    )
+}
---
+++ src/index.jsx
@@ -0,0 +1,44 @@
+import './style.css';
+import ReactDOM from 'react-dom/client';
+import { Canvas } from '@react-three/fiber';
+import Experience from './Experience.jsx';
+import { useState, useEffect } from 'react';
+
+function ResponsiveCanvas() {
+    const [camera, setCamera] = useState([0, 3, 7]);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth <= 768) {
+                setCamera([-5, 7, 16]); //mobile
+            } else {
+                setCamera([-5, 5, 16]);
+            }
+        };
+
+        handleResize(); // Inisialisasi posisi kamera berdasarkan ukuran layar
+        window.addEventListener('resize', handleResize); // Tambahkan listener untuk resize
+        return () => window.removeEventListener('resize', handleResize); // Bersihkan listener
+    }, []);
+
+    return (
+        <Canvas
+            shadows
+            camera={{
+                fov: 45,
+                near: 0.1,
+                far: 200,
+                position: camera, // Posisi kamera responsif
+            }}
+        >
+            <Experience />
+        </Canvas>
+    );
+}
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+// Render hanya sekali
+root.render(
+    <ResponsiveCanvas />
+);
